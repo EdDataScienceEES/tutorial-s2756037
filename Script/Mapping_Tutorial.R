9e134747d5ea941bc582446270765dcc519fc550
Anna Longenbaugh <annalongenbaugh@Annas-MacBook-Pro-134.local>
2024-11-26 13:51:27 +0000

    adding script

---
+++ Script/Mapping_Tutorial.R
@@ -0,0 +1,28 @@
+# Mapping Continued: Incorporating Intricate Details into Data Visualization
+# Created by: Anna Longenbaugh
+# Start Date: 26/11/24
+# End Data: 28/11/24
+
+# Load/Install Necessary Libraries
+install.packages("readr")
+install.packages("tidyr")
+install.packages("dplyr")
+install.packages("broom")
+install.packages("ggplot2")
+install.packages("ggExtra")
+install.packages("maps")
+install.packages("RColorBrewer")
+install.packages("raster")
+install.packages("rworldmap")
+
+library(readr)
+library(broom)
+library(ggplot2) 
+library(ggExtra)
+library(dplyr) 
+library(raster)  
+library(rworldmap)  
+library(RColorBrewer)
+library(maps)
+
+
