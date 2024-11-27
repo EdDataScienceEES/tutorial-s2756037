abf22417be204f1cb61500dbce5ec8c88bb3eef6
Anna Longenbaugh <annalongenbaugh@Annas-MacBook-Pro-134.local>
2024-11-27 20:05:34 +0000

    adding correct data

--- Script/Mapping_Tutorial.R
+++ Script/Mapping_Tutorial.R
@@ -25,4 +25,11 @@ library(rworldmap)
 library(RColorBrewer)
 library(maps)
 
+# Load Data
+Biodiversity_NY <- read.csv("Data/Biodiversity_by_County_-_Distribution_of_Animals__Plants_and_Natural_Communities.csv")
 
+# Tidying Data
+# Want to include Country, Project name, Species, and Hectares
+Biodiversity_NY_cleaned <- Biodiversity_NY[, setdiff(names(Biodiversity_NY), c("Taxonomic.Subgroup", "Scientific.Name", "Year.Last.Documented", "NY.Listing.Status", "State.Conservation.Rank", "Global.Conservation.Rank", "Distribution.Status"))]
+
+             
